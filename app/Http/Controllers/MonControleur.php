89fc0a13b31758d6b6942462046721688a8354c4
traccoenlea <leatraccoen@gmail.com>
2019-02-08 09:25:18 +0100

    fonctionnel

--- app/Http/Controllers/MonControleur.php
+++ app/Http/Controllers/MonControleur.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Chanson;
 use Illuminate\Http\Request;
 
 class MonControleur extends Controller
 {
     public function index() {
-        $chanson = Chanson::all();
+        $chansons = Chanson::all();
             //->orderBy("id desc");
-        return view("index", ["chansons"]=>$chansons);
+        return view("index", ["chansons"=>$chansons]);
     }
 }
