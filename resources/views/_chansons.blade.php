c55d6a0a7dc1062054ec615c01bf332132a7be2c
cloelh <cloelh@outlook.fr>
2019-03-18 10:53:15 +0100

    app.js

--- resources/views/_chansons.blade.php
+++ resources/views/_chansons.blade.php
@@ -1,5 +1,8 @@
 <ul id="listemusique">
     @foreach($chansons as $c)
-        <li class="musique"><a class="chanson" data-file="{{$c->fichier}}" href="#">{{$c->nom}}</a><div>ajoutée par <a href="/utilisateur/{{$c->utilisateur->id}}">{{$c->utilisateur->name}}</a></div> </li>
+        <li class="musique">
+            <div class="chanson" ><a data-file="{{$c->fichier}}" href="#">{{$c->nom}}</a></div>
+            <div class="name">ajoutée par <a href="/utilisateur/{{$c->utilisateur->id}}">{{$c->utilisateur->name}}</a></div>
+        </li>
     @endforeach
 </ul>
