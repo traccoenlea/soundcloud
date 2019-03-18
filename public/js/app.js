a3244c2d009e27ca7a226b18928fb807c0b897ca
traccoenlea <leatraccoen@gmail.com>
2019-03-18 10:50:23 +0100

    ajax

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
     //$(document).pjax('[data-pjax-toggle] a, a[data-pjax-toggle]', '#pjax-container', {push : false});
-    $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container');
+    //$(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container');
 
 
     $(".chanson").click(function(e) {
@@ -31,4 +31,27 @@ $(document).ready(function(){
             }
         });
     });
+
+
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    toastr["success"]("Inconceivable !");
+
+
 });
